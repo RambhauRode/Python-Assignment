0457094d34244f88ca8ae7623975f0e16aba60cf
RambhauRode <68366228+RambhauRode@users.noreply.github.com>
2020-07-24 11:11:33 +0530

    Add files via upload

---
+++ Day4/StringlowerUpper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+s1="what beautilful day it is"
+result1=s1.islower()
+print(result1)
+
+s2="What beautilful day it is"
+result2=s2.islower()
+print(result2)
+
+s3="what BeaUtilfuL day It Is"
+result3=s3.islower()
+print(result3)
+
+string1="What beautilful day it is"
+r1=string1.isupper()
+print(r1)
+
+string2="What BeaUtilfuL day It Is"
+r2=string2.isupper()
+print(r2)
+
+string3="WHAT A BEAUTIFUL DAY IT IS"
+r3=string3.isupper()
+print(r3)
+
+
+# In[ ]:
+
+
+
+
