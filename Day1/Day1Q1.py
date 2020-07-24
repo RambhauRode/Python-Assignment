610579a622627ca61bf3bbd8b51c2dc514ed18bd
RambhauRode <68366228+RambhauRode@users.noreply.github.com>
2020-07-24 11:26:23 +0530

    Add files via upload

---
+++ Day1/Day1Q1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # What is python? what are the ways to install python in your PC?
+Python is an interpreted ,high level,general purpose programming language.Created by Guado van Rossum and first released in 1991.
+Python having easier sytan,less amount of code,utilized by major technologcal companies
+steps for python installation:
+    open Ananconda Navigator
+    Lanch Jupyter NB
+    Create new 'Python 3NB'
+    Rename that 'untitled Folder' to LU Python ES Day1
+    Open Folder
+    create new Python 3 NB
+    Rename the notebook to LU Python ES Day1
+
+Python is easily installed on windows ,Linux,MacOS etc
+
+# # What are code environment Types ?
+Code environment is a place, where can write our code,compile that code.Performance of code at the same time
+There are 3 types of environment:
+    Text Editors
+    Full IDE's
+    Notebook Environment
